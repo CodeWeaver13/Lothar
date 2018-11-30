ba38e7048631ddde3ec500501559c4ed6cf27da5
汪诗雨 <wangshiyu@moqipobing.com>
2018-11-30 18:19:18 +0800

    1.0.7

--- Lothar/Source/Lothar.m
+++ Lothar/Source/Lothar.m
@@ -44,6 +44,14 @@
 }
 
 #pragma mark - public methods
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        _config = [[LotharConfig alloc] init];
+    }
+    return self;
+}
+
 + (instancetype)shared {
     static Lothar *mediator;
     static dispatch_once_t onceToken;
@@ -146,18 +154,12 @@
     }
     
     if ([target respondsToSelector:action]) {
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Warc-performSelector-leaks"
-        return [target performSelector:action withObject:params];
-#pragma clang diagnostic pop
+        return [self safeTarget:target performAction:action params:params];
     } else {
         // 这里是处理无响应请求的地方，如果无响应，则尝试调用对应target的notFound方法统一处理
         SEL notFound = NSSelectorFromString(@"notFound:");
         if ([target respondsToSelector:notFound]) {
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Warc-performSelector-leaks"
-            return [target performSelector:notFound withObject:params];
-#pragma clang diagnostic pop
+            return [self safeTarget:target performAction:notFound params:params];
         } else {
             // 如果action无法响应，且notFound未实现，则调用tipDelegate的notFoundActionTipController返回提示VC
             if ([self.tipDelegate respondsToSelector:@selector(notFoundActionTipController)]) {
@@ -168,6 +170,72 @@
         }
     }
     return nil;
+}
+
+- (id)safeTarget:(NSObject *)target performAction:(SEL)action params:(NSDictionary *)params {
+    NSMethodSignature* methodSig = [target methodSignatureForSelector:action];
+    if(methodSig == nil) {
+        return nil;
+    }
+    const char* retType = [methodSig methodReturnType];
+    
+    if (strcmp(retType, @encode(void)) == 0) {
+        NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:methodSig];
+        [invocation setArgument:&params atIndex:2];
+        [invocation setSelector:action];
+        [invocation setTarget:target];
+        [invocation invoke];
+        return nil;
+    }
+    
+    if (strcmp(retType, @encode(NSInteger)) == 0) {
+        NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:methodSig];
+        [invocation setArgument:&params atIndex:2];
+        [invocation setSelector:action];
+        [invocation setTarget:target];
+        [invocation invoke];
+        NSInteger result = 0;
+        [invocation getReturnValue:&result];
+        return @(result);
+    }
+    
+    if (strcmp(retType, @encode(BOOL)) == 0) {
+        NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:methodSig];
+        [invocation setArgument:&params atIndex:2];
+        [invocation setSelector:action];
+        [invocation setTarget:target];
+        [invocation invoke];
+        BOOL result = 0;
+        [invocation getReturnValue:&result];
+        return @(result);
+    }
+    
+    if (strcmp(retType, @encode(CGFloat)) == 0) {
+        NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:methodSig];
+        [invocation setArgument:&params atIndex:2];
+        [invocation setSelector:action];
+        [invocation setTarget:target];
+        [invocation invoke];
+        CGFloat result = 0;
+        [invocation getReturnValue:&result];
+        return @(result);
+    }
+    
+    if (strcmp(retType, @encode(NSUInteger)) == 0) {
+        NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:methodSig];
+        [invocation setArgument:&params atIndex:2];
+        [invocation setSelector:action];
+        [invocation setTarget:target];
+        [invocation invoke];
+        NSUInteger result = 0;
+        [invocation getReturnValue:&result];
+        return @(result);
+    }
+    
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Warc-performSelector-leaks"
+    return [target performSelector:action withObject:params];
+#pragma clang diagnostic pop
 }
 
 - (void)releaseTargetCacheWithTargetName:(NSString *)targetName {
