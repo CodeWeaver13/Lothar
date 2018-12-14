bcca929f6307a40308a387865b1678189f310fdc
郑永能 <1003580893@qq.com>
2018-12-14 17:21:09 +0800

    fix

--- Lothar/Source/LotharTipProtocol.h
+++ Lothar/Source/LotharTipProtocol.h
@@ -12,15 +12,16 @@
 /**
  外部调用URL参数错误时默认的提示VC
  */
-+ (nonnull UIViewController *)paramsErrorTipController;
+- (nonnull UIViewController *)paramsErrorTipController;
 
 /**
  内部调用找不到控制器时的提示VC
  */
-+ (nonnull UIViewController *)notFoundTargetTipController;
+- (nonnull UIViewController *)notFoundTargetTipController;
 
 /**
  内部调用无法解析参数时的提示VC
  */
-+ (nonnull UIViewController *)notFoundActionTipController;
+- (nonnull UIViewController *)notFoundActionTipController;
+
 @end
